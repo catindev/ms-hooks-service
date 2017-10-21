63a6007689d9c4eff112e28c11cd2e05b4f06971
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-21 14:57:06 +0600

    Call error

--- src/call-route.js
+++ src/call-route.js
@@ -1,11 +1,12 @@
+const CustomError = require('./utils/error')
+
 module.exports = (request, response) => {
     const required = [ 'clientNumber', 'trunkNumber' ]
     const payloadKeys = Object.keys(request.body)
     const fields = required.filter( key => payloadKeys.indexOf(key) === -1 )
 
-    if (fields.length > 0) return response.status(400).json({ 
-        status:400, message: 'required fields are missing', fields
-    })  
+    if (fields.length > 0) 
+        throw new CustomError(`Не заполнены обязательные параметры (${ fields.join(', ') })`, 400)
 
     response.json({ status: 200 })   
 }
