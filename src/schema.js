6546ae249818c10313522cd6a3f0426d12ee21e8
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-10-30 01:08:13 +0600

    fix schema

--- src/schema.js
+++ src/schema.js
@@ -63,7 +63,7 @@ const Call = mongoose.model('Call', new Schema({
     customer: { type: ObjectId, ref: 'Customer' },
     trunk: { type: ObjectId, ref: 'Trunk' },
     user: { type: ObjectId, ref: 'User' },
-    date: { type: Date, default: Date.now() },
+    date: { type: Date, default: new Date() },
     record: String,
     duration: {
         waiting: Number,
