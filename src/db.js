e206138c140893143fb8663418a8d517170d0c03
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2018-02-21 01:56:47 +0600

    Debug db connection

--- src/db.js
+++ src/db.js
@@ -4,11 +4,18 @@ const { log, warn } = console
 mongoose.Promise = Promise
 
 mongoose.connection.openUri('mongodb://ms3usr:mp7u@ds117876-a0.mlab.com:17876,ds117876-a1.mlab.com:17876/ms3?replicaSet=rs-ds117876')
-	.once('open', () => log('MongoDB connected'))
+	.once('open', () => {
+		log('MongoDB connected')
+		log('_________________')
+	})
 	.on('close', () => {
 		log('MongoDB connection closed')
+		log('_________________________')
 		process.exit(0)
 	})
-	.on('error', error => warn('Warning', error))
+	.on('error', error => {
+		warn('Warning', error)
+		log('_________________________')
+	})
 
 module.exports = mongoose
