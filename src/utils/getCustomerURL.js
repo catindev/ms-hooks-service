57617ffaf114d90b9f924435dd908e763d36aea5
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2018-02-26 00:54:30 +0600

    fix push URL prefix

--- src/utils/getCustomerURL.js
+++ src/utils/getCustomerURL.js
@@ -1,5 +1,5 @@
 module.exports = function ({ funnelStep, _id }) {
-    const prefix = 'beta'
+    const prefix = 'new'
     if (funnelStep === 'lead') return `http://${prefix}.mindsales-crm.com/leads/hot/${_id}`
     if (funnelStep === 'cold') return `http://${prefix}.mindsales-crm.com/leads/cold/${_id}`
     if (funnelStep === 'reject' || funnelStep === 'deal') return `http://${prefix}.mindsales-crm.com/closed/${_id}`
