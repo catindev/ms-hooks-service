269a658d2fba1e914cb07e5a5f646ffde30ff9a2
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2018-03-15 20:53:25 +0600

    fix test code

--- src/endpoints/endOfCall.js
+++ src/endpoints/endOfCall.js
@@ -3,11 +3,6 @@ const { Account, Trunk, User, Customer, Call, Contact } = require('../schema')
 const CustomError = require('../utils/error')
 const { addLog } = require('../logs')
 
-const Raven = require('raven')
-const { promisify } = require('util')
-
-const readFileAsync = promisify(fs.readFile)
-
 const formatNumber = require('../utils/formatNumber')
 // const formatNumber = (number, strictMode) => `+${number}`
 
